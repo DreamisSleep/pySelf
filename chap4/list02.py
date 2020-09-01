75fc079c990ecb06ac3e842e505567094d913bf6
HyunSeoPark <goatsfish@naver.com>
2020-09-02 06:24:12 +0900

    Python

---
+++ chap4/list02.py
@@ -0,0 +1,15 @@
+list_a = [1, 2, 3]
+
+print("# 리스트 뒤에 요소 추가하기")
+list_a.append(4)
+list_a.append(5)
+print(list_a)
+print()
+
+print("# 리스트 중간에 요소 추가하기")
+list_a.insert(0, 10)
+print(list_a)
+
+# 비파괴적, 파괴적의 차이
+# 한 번에 여러 요소를 추가 extend()
+
